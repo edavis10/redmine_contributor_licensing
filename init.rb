cbb1e8299273e0445f9023eac2757e1b26f8c7f3
Eric Davis <edavis@littlestreamsoftware.com>
2010-06-08 12:37:05 -0700

    [#4141] Generated documentation

--- init.rb
+++ init.rb
@@ -1,8 +1,11 @@
 require 'redmine'
 
 Redmine::Plugin.register :redmine_contributor_licensing do
-  name 'Redmine Contributor Licensing plugin'
-  author 'Author name'
-  description 'This is a plugin for Redmine'
-  version '0.0.1'
+  name 'Redmine Contributor Licensing'
+  author 'Eric Davis'
+  description 'Allows Redmine users to sign Contributor Licensing Agreements in Redmine.'
+  url 'https://projects.littlestreamsoftware.com/projects/redmine-cla'
+  author_url 'http://www.littlestreamsoftware.com'
+  version '0.1.0'
+  requires_redmine :version_or_higher => '0.9.0'
 end
