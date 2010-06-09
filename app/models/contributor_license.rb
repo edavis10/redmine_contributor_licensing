80588935a748fccee55ce7e276fda84cb76e1c3a
Eric Davis <edavis@littlestreamsoftware.com>
2010-06-09 11:24:12 -0700

    [#4146] Added a virtual accessor for tracking acceptance.

--- app/models/contributor_license.rb
+++ app/models/contributor_license.rb
@@ -1,9 +1,12 @@
 class ContributorLicense < ActiveRecord::Base
   unloadable
+
+  attr_accessor :acceptance
   
   belongs_to :user
 
   validates_inclusion_of :state, :in => %w(pending accepted), :allow_blank => false, :allow_nil => false
+  validates_format_of :acceptance, :with => /I agree/i, :on => :create
 
   def after_initialize
     self.state = 'pending' unless self.state.present?
@@ -11,15 +14,18 @@ class ContributorLicense < ActiveRecord::Base
 
   def accept!
     return self if state == 'accepted'
-    update_attributes({
-                        :state => 'accepted',
-                        :accepted_at => Time.now
-                      })
-    reload
+    self.state = 'accepted'
+    self.accepted_at = Time.now
+    self.save
   end
 
   def accepted?
     state == "accepted"
   end
   alias :accepted_contributor_license? :accepted?
+
+  if Rails.env.test?
+    generator_for :acceptance => 'I agree'
+  end
+  
 end
--- test/unit/contributor_license_test.rb
+++ test/unit/contributor_license_test.rb
@@ -10,9 +10,45 @@ class ContributorLicenseTest < ActiveSupport::TestCase
   should_allow_values_for(:state, "pending", "accepted")
   should_not_allow_values_for(:state, "", nil, "garbarge", "long" * 1000, 1000)
 
+  context "validations on acceptance" do
+    setup do
+      @license = ContributorLicense.new
+    end
+    
+    should "pass with 'I agree'" do
+      @license.acceptance = 'I agree'
+      assert @license.valid?
+    end
+    
+    should "pass with 'i AgrEe' (case insensitive)" do
+      @license.acceptance = 'i AgrEe'
+      assert @license.valid?
+    end
+    
+    should "pass with 'this is a contract i agree with' (extra text)" do
+      @license.acceptance = 'this is a contract i agree with'
+      assert @license.valid?
+    end
+
+    should "fail without 'i agree'" do
+      @license.acceptance = nil
+      assert_equal false, @license.valid?
+
+      @license.acceptance = ''
+      assert_equal false, @license.valid?
+
+      @license.acceptance = "I don't agree"
+      assert_equal false, @license.valid?
+
+      @license.acceptance = "garbarge" * 25
+      assert_equal false, @license.valid?
+    end
+    
+  end
+  
   context "accept!" do
     setup do
-      @license = ContributorLicense.generate!
+      @license = ContributorLicense.generate!(:acceptance => 'I agree')
     end
     
     should "update the state to 'accepted'" do
