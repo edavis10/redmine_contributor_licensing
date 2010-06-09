a6fe722430d6883de77ee2f04cdaa236833f6b88
Eric Davis <edavis@littlestreamsoftware.com>
2010-06-09 11:24:52 -0700

    [#4146] Allow a user to accept a CLA with the clickwrap agreement.

--- app/controllers/contributor_licenses_controller.rb
+++ app/controllers/contributor_licenses_controller.rb
@@ -2,6 +2,19 @@ class ContributorLicensesController < ApplicationController
   unloadable
   
   def show
-    render :text => 'TODO'
+    @contributor_license = ContributorLicense.new
+    @content = Setting.plugin_redmine_contributor_licensing['content']
+  end
+
+  def create
+    @content = Setting.plugin_redmine_contributor_licensing['content']
+
+    @contributor_license = User.current.build_contributor_license(params[:contributor_license])
+    if @contributor_license.accept!
+      flash[:notice] = l(:contributor_license_text_accepted)
+      redirect_to root_path
+    else
+      render :action => 'show'
+    end
   end
 end
--- test/integration/accepting_license_test.rb
+++ test/integration/accepting_license_test.rb
@@ -4,6 +4,7 @@ class AcceptingLicenseTest < ActionController::IntegrationTest
   include IntegrationTestHelper
 
   def setup
+    setup_plugin_configuration
     User.current = nil
     @user = User.generate!(:login => 'existing', :password => 'existing', :password_confirmation => 'existing', :admin => true)
   end
@@ -17,12 +18,58 @@ class AcceptingLicenseTest < ActionController::IntegrationTest
   
   should "not show the top menu link to users who have already accepted" do
     @license = ContributorLicense.generate!
-    @user.contributor_license = @license.accept!
+    @license.accept!
+    @user.contributor_license = @license
     
     login_as
     visit '/'
     assert_response :success
     assert_select "#top-menu a.contributor-license", :count => 0
   end
+
+  should "show the license content on the CLA page" do
+    login_as
+    visit '/'
+    click_link "Contributor License"
+    assert_equal '/contributor_licenses', current_url
+
+    assert_select 'div#license-content', :text => /Here is some content/
+  end
+  
+  should "allow entering 'I agree' to accept a CLA" do
+    login_as
+    visit '/'
+    click_link "Contributor License"
+    assert_equal '/contributor_licenses', current_url
+
+    fill_in "acceptance", :with => 'i AgRee with you'
+    click_button "Accept"
+
+    assert_equal 'http://www.example.com/', current_url
+    assert_select '.flash', :text => /accepted/i
+
+    license = @user.contributor_license
+    assert_equal 'accepted', license.state
+  end
+  
+  should "check that 'I agree' was used to accept a CLA" do
+    login_as
+    visit '/'
+    click_link "Contributor License"
+    assert_equal '/contributor_licenses', current_url
+
+    fill_in "acceptance", :with => 'junk'
+    click_button "Accept"
+
+    assert_equal '/contributor_licenses', current_url
+    assert_select '.errorExplanation', :text => /to accept/
+
+    assert_select 'div#license-content', :text => /Here is some content/
+
+    assert_nil @user.reload.contributor_license
+  end
+  
+
+  should "allow uploading a file to accept a CLA"
 end
 
