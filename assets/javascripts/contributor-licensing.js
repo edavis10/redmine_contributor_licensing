3c86fcaf1aef51e0d5e8eed0db99e76f7445ae1b
Eric Davis <edavis@littlestreamsoftware.com>
2010-06-14 11:29:32 -0700

    [#4143] Highlight the license state on the revision details page.

--- assets/javascripts/contributor-licensing.js
+++ assets/javascripts/contributor-licensing.js
@@ -1,8 +1,15 @@
 jQuery(function($) {
+  wrapWithContributorColor = function(contributorState) {
+    return '<span class="'+contributorState+'" title="'+contributorState+'"></span>';
+  };
+
   $.each(contributors, function(index, contributor) {
-    $('#content td:contains("'+contributor.name+'")').
-      wrapInner(function() {
-        return '<span class="'+contributor.state+'" title="'+contributor.state+'"></span>'
-      });
+    $('#content td:contains("'+contributor.name+'")').wrapInner(function() {
+      return wrapWithContributorColor(contributor.state);
+    });
+
+    $('#content .author a:contains("'+contributor.name+'")').wrapInner(function() {
+      return wrapWithContributorColor(contributor.state);
+    });
   });
 });
