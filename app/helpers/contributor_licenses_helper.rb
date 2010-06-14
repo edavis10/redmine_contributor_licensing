3d0375b6aac940178401560226942001e5c92c5b
Eric Davis <edavis@littlestreamsoftware.com>
2010-06-14 09:07:41 -0700

    [#4144] Added a navigation menu to the admin panel.

---
+++ app/helpers/contributor_licenses_helper.rb
@@ -0,0 +1,17 @@
+module ContributorLicensesHelper
+  def contributor_licenses_menu(actions, contributor_license = nil)
+    menu_items = []
+    menu_items << link_to(l(:contributor_licensing_label_plural), contributor_licenses_path, :class => 'icon icon-multiple') if actions.include?(:index)
+
+    if contributor_license
+      menu_items << link_to(l(:contributor_licensing_label), contributor_license_path(contributor_license), :class => 'icon icon-document') if actions.include?(:show)
+
+      menu_items << link_to(l(:contributor_licensing_label_approve), approve_contributor_license_path(contributor_license), :method => :put, :class => 'icon icon-edit') unless contributor_license.accepted? && actions.include?(:delete)
+      
+      menu_items << link_to(l(:button_delete), contributor_license_path(contributor_license), :confirm => l(:text_are_you_sure), :method => :delete, :class => 'icon icon-del') if actions.include?(:delete)
+    end
+    
+    return content_tag(:div, menu_items.join(' '), :class => "contextual")
+
+  end
+end
